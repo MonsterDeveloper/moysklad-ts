f7304882836b158c83fcc7bee717557f5f6205f7
Andrei Bobkov <slider7259@gmail.com>
2023-09-28 16:04:57 +0200

    feat(compose-search-parameters.test.ts): add tests for expand option fix(compose-search-parameters.ts): add support for expand option

--- packages/moysklad-ts/src/api-client/compose-search-parameters.test.ts
+++ packages/moysklad-ts/src/api-client/compose-search-parameters.test.ts
@@ -2,26 +2,68 @@ import { describe, it, expect } from "vitest";
 import { composeSearchParameters } from "./compose-search-parameters";
 
 describe("composeSearchParameters", () => {
-  it("should return undefined if no pagination options are provided", () => {
+  it("should return undefined if no options are provided", () => {
     expect(composeSearchParameters({})).toBeUndefined();
   });
 
-  it("should return a URLSearchParams object with 'limit' parameter if 'limit' is provided in pagination options", () => {
-    const pagination = { limit: 10 };
-    const searchParameters = composeSearchParameters({ pagination });
-    expect(searchParameters?.get("limit")).toBe("10");
+  describe("pagination", () => {
+    it("should return a URLSearchParams object with 'limit' parameter if 'limit' is provided in pagination options", () => {
+      const pagination = { limit: 10 };
+      const searchParameters = composeSearchParameters({ pagination });
+      expect(searchParameters?.get("limit")).toBe("10");
+    });
+
+    it("should return a URLSearchParams object with 'offset' parameter if 'offset' is provided in pagination options", () => {
+      const pagination = { offset: 20 };
+      const searchParameters = composeSearchParameters({ pagination });
+      expect(searchParameters?.get("offset")).toBe("20");
+    });
+
+    it("should return a URLSearchParams object with both 'limit' and 'offset' parameters if both are provided in pagination options", () => {
+      const pagination = { limit: 10, offset: 20 };
+      const searchParameters = composeSearchParameters({ pagination });
+      expect(searchParameters?.get("limit")).toBe("10");
+      expect(searchParameters?.get("offset")).toBe("20");
+    });
   });
 
-  it("should return a URLSearchParams object with 'offset' parameter if 'offset' is provided in pagination options", () => {
-    const pagination = { offset: 20 };
-    const searchParameters = composeSearchParameters({ pagination });
-    expect(searchParameters?.get("offset")).toBe("20");
-  });
+  describe("expand", () => {
+    it("should not add 'expand' parameter if 'expand' is an empty object", () => {
+      const searchParameters = composeSearchParameters({ expand: {} });
+      expect(searchParameters).toBeUndefined();
+    });
 
-  it("should return a URLSearchParams object with both 'limit' and 'offset' parameters if both are provided in pagination options", () => {
-    const pagination = { limit: 10, offset: 20 };
-    const searchParameters = composeSearchParameters({ pagination });
-    expect(searchParameters?.get("limit")).toBe("10");
-    expect(searchParameters?.get("offset")).toBe("20");
+    it("should add 'expand' parameter with correct fields if 'expand' is not empty", () => {
+      const expand = {
+        owner: false,
+        group: true,
+        demand: {},
+        agent: {
+          owner: true,
+          account: {
+            group: true,
+          },
+        },
+      };
+      const searchParameters = composeSearchParameters({ expand });
+      expect(searchParameters?.toString().replaceAll("%2C", ",")).toBe(
+        "expand=group,demand,agent.owner,agent.account.group",
+      );
+    });
+
+    it("should throw an error if 'expand' is nested more than 3 levels deep", () => {
+      const expand = {
+        level1: {
+          level2: {
+            level3: {
+              level4: true,
+            },
+          },
+        },
+      };
+      expect(() => composeSearchParameters({ expand })).toThrow(
+        "Expand depth cannot be more than 3",
+      );
+    });
   });
 });
--- packages/moysklad-ts/src/api-client/compose-search-parameters.ts
+++ packages/moysklad-ts/src/api-client/compose-search-parameters.ts
@@ -1,9 +1,40 @@
 import type { PaginationOptions } from "@/types";
 
+function traverseExpand(expand: Record<string, unknown>, depth = 0) {
+  if (depth > 2) {
+    throw new Error("Expand depth cannot be more than 3");
+  }
+
+  const fields: string[] = [];
+
+  for (const [key, value] of Object.entries(expand)) {
+    if (!value) continue;
+
+    if (
+      typeof value === "object" &&
+      value !== null &&
+      Object.keys(value).length > 0
+    ) {
+      const subFields = traverseExpand(
+        value as Record<string, unknown>,
+        depth + 1,
+      );
+      for (const subField of subFields) fields.push(`${key}.${subField}`);
+      continue;
+    }
+
+    fields.push(key);
+  }
+
+  return fields;
+}
+
 export function composeSearchParameters({
   pagination,
+  expand,
 }: {
   pagination?: PaginationOptions;
+  expand?: Record<string, unknown>;
 }) {
   const searchParameters = new URLSearchParams();
 
@@ -11,6 +42,12 @@ export function composeSearchParameters({
     searchParameters.append("limit", pagination.limit.toString());
   if (pagination?.offset)
     searchParameters.append("offset", pagination.offset.toString());
+
+  if (expand) {
+    const expandFields = traverseExpand(expand);
+    if (expandFields.length > 0)
+      searchParameters.append("expand", expandFields.join(","));
+  }
 
   return searchParameters.size > 0 ? searchParameters : undefined;
 }
