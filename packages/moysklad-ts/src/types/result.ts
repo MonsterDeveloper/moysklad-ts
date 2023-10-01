4641d7be1cd22965d0ac7849486fd8bbd68fe395
Andrei Bobkov <slider7259@gmail.com>
2023-10-02 00:03:47 +0200

    feat(result.ts): add support for nested expand options
    The type definition for GetFindResult in result.ts has been improved
    to handle nested expand options. The previous implementation had a
    placeholder comment for handling nested expand options, which has now
    been replaced with the actual implementation. This change allows for
    more flexibility and accuracy when using the expand option in the
    Moysklad API.

--- packages/moysklad-ts/src/types/result.ts
+++ packages/moysklad-ts/src/types/result.ts
@@ -19,22 +19,21 @@ export type GetFindResult<P extends Payload, E> = IsEqual<E, any> extends true
           ? K extends keyof P["object"]
             ? P["expandable"][K] extends Payload
               ? E[K] extends object
-                ? // expand option is an object
-                  // GetFindResult<P["expandable"][K], E[K]>
-                  // TODO add nested expand
-                  "object - todo"
+                ? // expand option is an object (nested expand)
+                  GetFindResult<P["expandable"][K], E[K]>
                 : // expand option is `true`
                 P["object"][K] extends ListMeta<infer O>
                 ? // expandable field is a ListMeta
                   ListMeta<O> & { rows: P["expandable"][K]["object"][] }
                 : NonNullable<P["object"][K]> extends Array<unknown>
-                ? P["expandable"][K]["object"][]
+                ? // expandable field is an array
+                  P["expandable"][K]["object"][]
                 : P["expandable"][K]["object"]
               : never
             : never
           : never;
       }
     > &
-      // exclude expanded properties from original payload
+      // exclude expanded properties from original payload's object
       Omit<P["object"], ConditionalKeys<E, true | object>>
   : P["object"];
