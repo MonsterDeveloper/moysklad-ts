ecf82ba38b42d8f68aa9b431f6854c71420cfd20
Andrei Bobkov <slider7259@gmail.com>
2023-10-02 16:34:21 +0200

    feat(payload.ts): add GetPayloadUpdatableFields type
    The GetPayloadUpdatableFields type is used to extract the non-readonly
    fields from the object property of the Payload type. It replaces the
    Meta type with the UpdateMeta type. This type is useful for updating
    specific fields of an object in the payload. If a field is optional, it
    can accept null values.

--- packages/moysklad-ts/src/types/payload.ts
+++ packages/moysklad-ts/src/types/payload.ts
@@ -1,3 +1,6 @@
+import type { WritableKeysOf } from "type-fest";
+import type { UpdateMeta, Meta } from "./metadata";
+
 export interface Payload<T extends object = object> {
   object: T;
   expandable: {
@@ -7,3 +10,19 @@ export interface Payload<T extends object = object> {
   };
   orderable?: string;
 }
+
+export type GetPayloadUpdatableFields<T extends Payload> = {
+  // extract non-readonly fields from payload's object and replace Meta with UpdateMeta
+  [Key in Exclude<
+    WritableKeysOf<T["object"]>,
+    "meta"
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  >]?: Meta<any> extends T["object"][Key]
+    ? undefined extends T["object"][Key]
+      ? UpdateMeta | null
+      : UpdateMeta
+    : // if key is optional, make it accept null
+    undefined extends T["object"][Key]
+    ? T["object"][Key] | null
+    : T["object"][Key];
+};
