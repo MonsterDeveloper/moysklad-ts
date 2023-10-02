0ed8cabc589bd3cb1c678212308f87871b99e4d1
Andrei Bobkov <slider7259@gmail.com>
2023-10-02 17:27:17 +0200

    test(endpoint.test.ts): add unit test for TokenEndpoint.create() method
    test(vitest-setup.ts): add mock server handlers for TokenEndpoint.create() method

--- packages/moysklad-ts/vitest-setup.ts
+++ packages/moysklad-ts/vitest-setup.ts
@@ -2,8 +2,10 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 
+const MOYSKLAD_BASE_URL = "https://api.moysklad.ru/api/remap/1.2";
+
 export const restHandlers = [
-  rest.all("https://api.moysklad.ru/api/remap/1.2", (_, response, context) => {
+  rest.all(MOYSKLAD_BASE_URL, (_, response, context) => {
     return response(
       context.status(200),
       context.json({ moysklad: "default api endpoint" }),
@@ -12,6 +14,9 @@ export const restHandlers = [
   rest.get("https://example.com/api", (_, response, context) => {
     return response(context.status(200), context.json({ example: "api" }));
   }),
+  rest.post(`${MOYSKLAD_BASE_URL}/security/token`, (_, response, context) =>
+    response(context.json({ access_token: "supersecret" })),
+  ),
 ];
 
 const server = setupServer(...restHandlers);
